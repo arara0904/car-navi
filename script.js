1496767f1778318bae424d0bd1c45cce4994a48e
arara0904 <79354953+arara0904@users.noreply.github.com>
2023-10-30 13:37:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 alert('周りに気を付けて走行しましょう');
 
+var watch = navigator.geolocation.watchPosition(function (position) {return position});
+if(!watch){alert("not found")}else{alert("found")}
+navigator.geolocation.clearWatch(watch);
+
 
 
 var i;
